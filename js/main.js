312213333b624334002f4b6a4a64a3a54f8c4d5f
miguelgc95 <miguelgc95@gmail.com>
2020-12-17 17:54:22 +0100

    navs well positioned

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-document.getElementsByTagName("body")addEventListener("click", triv)
+window.onload= trigger();
 
-function triv(){
-    console.log("wola");
+function trigger(){
+    var parent = document.querySelectorAll(".flow-btn");
+    for(i=0;i<parent.length;i++){
+        parent[i].addEventListener("click", flow)
+    }
+}
+
+function flow(e){
+    var parent = document.querySelectorAll("section");
+    console.log(parent[0].id);
+    for(i=0;i<parent.length;i++){
+        if(parent[i].id===e.target.name){
+            if(parent[i].classList.contains("hide-me")){
+                parent[i].classList.remove("hide-me");
+            }
+        }
+        else{
+            if(!parent[i].classList.contains("hide-me")){
+                parent[i].classList.add("hide-me")
+            }
+        }
+    }
 }
